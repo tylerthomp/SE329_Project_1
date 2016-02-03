c58b396ed057079130a81b704f8ce1dd797ded61
Tyler <tthomp@iastate.edu>
2016-02-02 20:09:03 -0600

    uploading is working

--- face_upload/views.py
+++ face_upload/views.py
@@ -7,7 +7,7 @@ import os
 
 
 def index(request):
-    return HttpResponse("Hello World   " + upload(request))
+    return HttpResponse("Hello World\n\n   " + upload(request))
 
 def upload(request):
     __location__ = os.path.realpath(
@@ -17,8 +17,8 @@ def upload(request):
         encoded_string = image_file.read().encode("base64")
 
     post_data = {'api_key': 'd45fd466-51e2-4701-8da8-04351c872236',
-                 'api_secret':'171e8465-f548-401d-b63b-caf0dc28df5f',
-                 'imagfile_data': encoded_string,
+                 'api_secret': '171e8465-f548-401d-b63b-caf0dc28df5f',
+                 'imagefile_data': encoded_string,
                  'original_filename': 'photo.jpg'}
     r = requests.post('http://www.betafaceapi.com/service_json.svc/UploadNewImage_File', post_data)
-    return r.content
+    return r.content + "\n Base64 Encoded Photo:\n" + encoded_string
