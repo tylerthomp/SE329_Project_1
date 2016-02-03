d52e65cf4ffccf24002527d3fc3cbb73a27882ff
Tyler <tthomp@iastate.edu>
2016-02-02 22:36:12 -0600

    added comments

--- face_upload/BetaFaceApiWrapper.py
+++ face_upload/BetaFaceApiWrapper.py
@@ -15,6 +15,7 @@ class BetaFaceApiWrapper:
     def api_secret(self):
         return '171e8465-f548-401d-b63b-caf0dc28df5f'
 
+    """ Takes a photo path relative to /SE321_Project_1/face_upload directory and returns an HttpResponse object. """
     def upload_image(self, relative_photo_path):
         location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) + "/" + relative_photo_path
         with open(location, "rb") as image_file:
@@ -25,6 +26,7 @@ class BetaFaceApiWrapper:
                      'original_filename': relative_photo_path}
         return requests.post(self.base_url() + 'UploadNewImage_File', post_data)
 
+    """ Takes a uid returned in upload_image()'s response and gives you info about the photo. """
     def get_image_info(self, uid):
         post_data = {'api_key': self.api_key(),
                      'api_secret': self.api_secret(),
--- face_upload/views.py
+++ face_upload/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from BetaFaceApiWrapper import BetaFaceApiWrapper
 import json
-
 # Create your views here.
-
 
 
 def index(request):
