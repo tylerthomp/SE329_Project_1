4cd09a88dc3b0fb5cd17af82b1a7714c57f8cd06
Tyler Thompson <tthomp@mac-air-15.local>
2016-02-02 19:56:14 -0600

    attempting to upload photo

--- face_upload/views.py
+++ face_upload/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import os
 # Create your views here.
 
 
 
 def index(request):
-    return HttpResponse("Hello World. You're at the upload index.")
+    return upload(request)
+
+def upload(request):
+    __location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__))) + '/photo.jpg'
+    encoded_string = None
+    with open(__location__, "rb") as image_file:
+        encoded_string = image_file.read().encode("base64")
+
+    post_data = {'api_key': 'd45fd466-51e2-4701-8da8-04351c872236',
+                 'api_secret':'171e8465-f548-401d-b63b-caf0dc28df5f',
+                 'imagfile_data': encoded_string,
+                 'original_filename': 'photo.jpg'}
+    r = requests.post('http://www.betafaceapi.com/service.svc/UploadNewImage_File', post_data)
+    return HttpResponse(r.content)
